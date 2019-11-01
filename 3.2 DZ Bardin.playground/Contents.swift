6af17e776daabb7adde617c011b36394d17b5ad5
Bardincom <master_als@icloud.com>
2019-11-01 17:10:16 +0200

    DZ 3.2 Solved the first task
    Added
    + Tuple myFavorite
    + Tuple myBelovedWife
    + intermediateTuple
    + spesialNumbers Tuple

--- 3.2 DZ Bardin.playground/Contents.swift
+++ 3.2 DZ Bardin.playground/Contents.swift
@@ -15,3 +15,28 @@ import UIKit
  5. Создайте новый кортеж, элементами которого будут любимое число из первого кортежа, любимое число из второго кортежа и разница любимых чисел первого и второго кортежей.
  */
 
+// Условие №1
+var myFavorite = (film: "Garry Potter", number: 19, dish: "Cake Surprise") // создаю кортеж с тремя параметрами
+
+// Условие №2
+let (favoriteFilm, favoriteNumber, favoriteEda) = myFavorite // записываю одним выражением в три константы
+
+// Условие №3
+var myBelovedWife = (film: "Just go whit it", number: 17, dish: "Cesar salad") // создаю втрой кортеж с налогичными первому параметрами
+
+// Условие №4
+var intermediateTuple: (String, Int, String) // промежуточный кортеж
+intermediateTuple = myFavorite // присваиваю промежуточному кортежу данные первого кортежа
+myFavorite = myBelovedWife // присваиваю первому кортежу данные второго кортежа
+myBelovedWife = intermediateTuple // присваивают второму кортежу данные промежуточного кортежа
+
+print("\t Итоговый результат присвоений:")
+print("myFavorite - Фильм: \(myFavorite.film), Число: \(myFavorite.number), Блюдо: \(myFavorite.dish)")
+print("myBelovedWife - Фильм: \(myBelovedWife.film), Число: \(myBelovedWife.number), Блюдо: \(myBelovedWife.dish)")
+
+
+// Условие №5
+let spesialNumbers = (myFavorite.number, myBelovedWife.number, myFavorite.number - myBelovedWife.number)
+spesialNumbers.0
+spesialNumbers.1
+spesialNumbers.2

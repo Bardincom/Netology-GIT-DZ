bfe33d7f0306ffae34b4bf54a90cee3570532724
Bardincom <master_als@icloud.com>
2019-11-10 00:20:09 +0200

    4.1 Solved the second task
    + Complete the second task
    + Changes in the first task
    + General improvements

--- 4.1 DZ Bardin.playground/Contents.swift
+++ 4.1 DZ Bardin.playground/Contents.swift
@@ -9,7 +9,13 @@ import UIKit
 ///Наследование
 ///Полиморфизм
 
-//Абстракция
+
+//MARK: Абстракция
+/// Лифт - нам не важно какими механизмами он приводится в движение, сколько дверей или двигателей у него, не важно какие задачи выполняются когда мы когда мы выполням простые действия вызов лифта и выбор этажа.
+protocol Actions{
+  func callTheElevator()
+  func chooseFloor()
+}
 
 class Elevatot: Actions{
   let door = 2 // кол. дверей
@@ -23,31 +29,25 @@ class Elevatot: Actions{
   func moveDown() {} // двигаться вниз
   func stopMotion() {} // прекратить движение
 }
-/// Лифт - нам не важно какими механизмами он приводится в движение, сколько дверей или двигателей у него, не важно какие задачи выполняются когда мы когда мы выполням простые действия вызов лифта и выбор этажа.
-protocol Actions{
-  func callTheElevator()
-  func chooseFloor()
-}
+
 
 // Мероприятие, концерт
-class Event: Visitor {
-  func toBuyTicket() {} // купить билет
-  func presentTicket() {} // предьявить билет
-  func takeYourPlace() {} // занять свое место
-  func perfomingArtists() {} // выступающие артисты
-  func oderOfPerfomance() {} // порядок выступления
-  let seatsInTheHall = 300  // кол. мест в зале
-}
-
-/// для посетителя важно только купить билет, предьявить его на входе и занять свое место
+//
 protocol Visitor {
-  func toBuyTicket()
-  func presentTicket()
-  func takeYourPlace()
+  func attendEvent() // посетить мероприятие
+  func chooseTheRightClothes() // выбрать подходящий наряд/одежу
+}
+
+class Event: Visitor {
+  func attendEvent() {}
+  func chooseTheRightClothes() {}
+  func perfomingArtists() {} // выступающие артисты информация
+  func oderOfPerfomance() {} // порядок выступления
+  let numberOfVisitors = 300  // кол. посетителей
 }
 
 
-//Инкапсуляция
+//MARK: Инкапсуляция
 class Woman {
   func sayName() {} // говорит как зовут
   func flirt() {} // флиртует
@@ -63,6 +63,7 @@ someWomen.askWhich(iphone: "Какой у тебя iPhone?")
 
 /// Моторное масло для автомобилей Тойота продается под тем же брендом, однако сама фирма не производит моторное масло. в данному условии мы предполагам что нам необходимо скрыть информацию о реальном производителе, а именно компании Mobil, чтобы продолжать продавать данный товар под маркой Тойота
 
+
 class MotorOilToyota {
   let brand = "Toyota"
   let category = "Cars"
@@ -72,8 +73,7 @@ class MotorOilToyota {
 }
 
 
-//Наследование
-
+//MARK: Наследование
 //Класс Письмо
 class Letter {
   func writeLetter () {} // написать письмо
@@ -96,21 +96,37 @@ class WorkEmail: Email {
 }
 
 
-/// Закрытая вечеринка - в этом примере наш подкласс наследуется от супер класса Event из примера Абстракция
+// Закрытая вечеринка - в этом примере наш подкласс наследуется от супер класса Event из примера Абстракция
 class PrivateParty: Event {
-  override func toBuyTicket() {
-    print("Проход по спискам приглашенных, билет купить нельзя")
-  }
-  override func presentTicket() {
-    print("Сказать фамилию Джобс")
-  }
-  override func takeYourPlace() {
-    print("Раслибиться и налить себе что-нибудь")
+  func passFaceControl () {}
+  override func chooseTheRightClothes() {
+    print("Вечеринка без галстуков")
   }
 }
 
 
-//Полиморфизм
+// упрощенный пример наследования:
+class Parents { // класс Родители
+  let surname = "Bardin" // свойство Фамилия
+}
+
+// при рождении ребенок наследует Фамилию родителей
+class Child: Parents {
+  let name = "Danya"
+  
+  func printFulName() {
+    print("My name's \(surname) \(name)")
+  }
+  
+}
+
+let child = Child()
+child.surname // мы можем обратиться к свойству Фамилия, которая была создана у родительского класса.
+child.name // а так же по собственному свойству
+child.printFulName() // метод который просто печает имя и фамилию ребенка, фамилия берется от родительского класса, имя из свойства собстенного класса
+
+
+//MARK: Полиморфизм
 // создадим объекты классов Письмо, Электронное письмо и Рабочее письмо
 let letter = Letter()
 let email = Email()
@@ -125,7 +141,7 @@ class Wheel { // абстрактный класс
   func pumpUpOnTheWheel () {} // некий абстрактный метовд накачать колесо
 }
 
-/// ниже выполняется принцип Полиморфизма при котором мы можем использовать, как в нашем примере, методов для выполнения внешне сходих, но технически разных задачь.
+/// ниже выполняется принцип Полиморфизма при котором мы можем использовать методы для выполнения внешне схожих, но для реализации используеющих разное оборудование
 class BicycleWheel: Wheel {
   override func pumpUpOnTheWheel() {
     print("Накачиваем ножным насосом")
@@ -143,4 +159,68 @@ class TractorWheel: Wheel {
     print("Накачиваем промышленным насосом")
   }
 }
+
+
+//MARK: Задача №2
+///Для каждого базового понятия ООП и приведите 2-3 примера из жизни. Опишите их текстом.
+///Класс
+///Объект
+///Свойства объектов
+///Методы
+///Интерфейс
+///Композиция и агрегация
+
+//MARK: Пример №1
+//пример Интерфейса, которому должна соответсововать лампа
+protocol Grow {
+  var kelvinColourEffeciency: Int { get }
+  var isWorks: Bool { get }
+}
+
+class Lamp: Grow { // класс Лампa
+  let kelvinColourEffeciency = 2700 // свойство объекта цветовая температура в кельвинах
+  let lampEfficiencyInPersent = 50 // свойство объекта КПД в процентах
+  let hoursServiceLife = 1000 // срок службы в часах
+  let isWorks = true // в рабочем состоянии
+  let shape: String = "Грушевидная"
+  
+  func toIlluminate () {} // метод Освещать что-либо
+}
+
+let firstLamp = Lamp() // объект класса Лампa
+firstLamp.toIlluminate() // в данном примере Объекту доступны все свойства и методы класса Лампа
+firstLamp.shape
+
+// пример агрегации и композиции
+class Chandelier { // Люстра
+  let lamp =  Lamp()
+  
+  func changeBrightness() {} // можем регулировать яркость освещения
+}
+
+let secondLamp = Lamp() // Лампа может существовать и без Люстры - это пример Агрегации
+let chandelier = Chandelier() // Люстра работать без Лампы не сможет - это пример Композиции
+
+
+//MARK: Пример №2
+//Пример интерфейса - наличие лицензии на медицинскую практику
+protocol MedicalLicense {
+  var isLicense: Bool { get } // наличие лицензии на ведение медицинской практики
+}
+
+class Doctor: MedicalLicense{ // класс Доктор
+  var isLicense = true // свойство
+  func toExaminePatients () {} // метод -  проводит осмотр пациентов
+  func keepMedicalHistory() {} // метод - вести историю болезни
+}
+
+// пример агрегации и композиции, для этого создам дополнительный класс Hospital
+
+class Hospital {
+  let doctor = Doctor() // в врачи работающие в Больницу
+  func diagnosisOfDiseases() {} // диагностика заболевания
+}
+
+let doctorHouse = Doctor() // объект класса Doctors - наш объект может существовать вне зависмости от того есть ли у нас Больница - пример агрегации
+let doctorHouseHospital = Hospital() // больница работать без докторов не сможет - пример композиции
 

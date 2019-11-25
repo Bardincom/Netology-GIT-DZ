f4d26a4e143832ca6a092a4d56357ee781eeb7cb
Bardincom <master_als@icloud.com>
2019-11-25 12:18:53 +0200

    4.4 Upgrade
    added check for exiting the range of array indices
    other improvements

--- 4.4 DZ Bardin.playground/Contents.swift
+++ 4.4 DZ Bardin.playground/Contents.swift
@@ -13,22 +13,24 @@ import UIKit
  */
 
 struct Track {
-  var nameTrack: String
+  var name: String
   var artist: String
   var duration: Float
   var country: String
 }
 
 class Category {
-  let nameCategory: String
+  let name: String
   
   lazy var trackList: [Track] = []
 
-  var numbersOfTrack: Int { // вычисляем кол. треков в категории
+  // вычисляем кол. треков в категории
+  var numbersOfTrack: Int {
     trackList.count
   }
   
-  var totalDuration: Float { // вычисляем общую продолжительность воспроизводимых треков
+  // вычисляем общую продолжительность воспроизводимых треков
+  var totalDuration: Float {
     var durations: Float = 0.0
     for item in trackList {
       durations += item.duration
@@ -37,27 +39,53 @@ class Category {
     return durations
   }
 
-  init(nameCategory: String) {
-    self.nameCategory = nameCategory
+  init(name: String) {
+    self.name = name
   }
   
-  func addNew(track: Track) { // добавляем треки
+  // добавляем треки
+  func addNew(track: Track) {
     trackList.append(track)
   }
-  func removeTrack(numder: Int) { // удаляем треки
-    trackList.remove(at: numder - 1) // для того чтобы пользователь мог в привычной форме удалять треки по номеру нахождения в списке добавлю формулу: значение минус 1 будет индекс под которым находится запрашиваемый трек
+  
+  // удаляем треки
+  func removeTrack(number: Int) {
+    guard number != 0  else {
+      print("ERROR: Введенный номер отсутствует")
+      return
+    }
+    guard (number - 1) < trackList.count else {
+      print("ERROR: Введенный номер отсутствует")
+      return
+    }
+
+    // для того чтобы пользователь мог в привычной форме удалять треки по номеру нахождения в списке добавлю формулу: значение минус 1, будет индекс под которым находится запрашиваемый трек
+    trackList.remove(at: number - 1)
   }
 }
 
-// Проверка
-var categoryRock = Category(nameCategory: "Rock")
+//MARK: Проверка №1
+var categoryRock = Category(name: "Rock")
 print("До добавления Треков: \(categoryRock.trackList) \n")
-categoryRock.addNew(track: Track(nameTrack: "Титаник", artist: "Наутилус", duration: 5.16, country: "RU"))
-categoryRock.addNew(track: Track(nameTrack: "Воздух", artist: "Наутилус", duration: 5.03, country: "RU"))
-categoryRock.addNew(track: Track(nameTrack: "Тутанхамон", artist: "Наутилус", duration: 4.51, country: "RU"))
+
+categoryRock.addNew(track: Track(name: "Титаник",
+                                 artist: "Наутилус",
+                                 duration: 5.16,
+                                 country: "RU"))
+
+categoryRock.addNew(track: Track(name: "Воздух",
+                                 artist: "Наутилус",
+                                 duration: 5.03,
+                                 country: "RU"))
+
+categoryRock.addNew(track: Track(name: "Тутанхамон",
+                                 artist: "Наутилус",
+                                 duration: 4.51,
+                                 country: "RU"))
+
 print("После добавления Треков: \(categoryRock.trackList) \n")
 categoryRock.totalDuration
-categoryRock.removeTrack(numder: 1)
+categoryRock.removeTrack(number: 4)
 print("После удаления Треков: \(categoryRock.trackList) \n")
 
 //MARK: Задача №2
@@ -68,11 +96,14 @@ print("После удаления Треков: \(categoryRock.trackList) \n")
 class Library {
   var nameLibrary: String
   lazy var categoryList: [Category] = []
-  var numbersOfCategoru: Int { // вычисляем кол. категорий
+  
+  // вычисляем кол. категорий
+  var numbersOfCategoru: Int {
     categoryList.count
   }
   
-  var durationLibrary: Float { // вычисляем общyю продолжительность всей библиотеки
+  // вычисляем общyю продолжительность всей библиотеки
+  var durationLibrary: Float {
     var duration: Float = 0
     
     for item in categoryList {
@@ -94,9 +125,11 @@ class Library {
     categoryList.remove(at: number - 1)
   }
   
-  func displayAllTracks() { // выводим в консоль треки разбитые по категориям
-    for category in categoryList { // обходим по массиву категория
-      print("\t Категрия: \(category.nameCategory), продолжительность звучания: \(category.totalDuration) мин") // выводим в консоль названия категория
+  // выводим в консоль треки разбитые по категориям
+  func displayAllTracks() {
+    // обходим по массиву категория
+    for category in categoryList {
+      print("\t Категрия: \(category.name), продолжительность звучания: \(category.totalDuration) мин") // выводим в консоль названия категория
       for tracks in category.trackList { // внутри категорий обходим трек листы
         print(tracks) // выводим название треков для каждой их категорий
       }
@@ -104,18 +137,34 @@ class Library {
     print("Общая продолжительность звучания библиотеки: \"\(nameLibrary)\"  \(durationLibrary) мин")
   }
   
-  func transferTrack(from categoryOne: Category, to categoryTwo: Category, track number: Int) {
+  func transferTrack(from categoryOne: Category,
+                     to categoryTwo: Category,
+                     track number: Int) {
+    guard number != 0 else {
+      print("ERROR: Выбранный трек отсутствует")
+      return
+    }
+    guard (number - 1) < categoryOne.trackList.count else {
+      print("ERROR: Выбранный трек отсутствует")
+      return
+    }
     print("Запрашиваемый трек для переноса: \(categoryOne.trackList[number - 1])")
     categoryTwo.addNew(track: categoryOne.trackList[number - 1]) // переношу запрашиваемый трек в другую категорию
-    categoryOne.removeTrack(numder: number) // удаляю из первой категории перенесенный трек
+    categoryOne.removeTrack(number: number) // удаляю из первой категории перенесенный трек
   }
 
 }
 
-
-var categoryLirics = Category(nameCategory: "Lirics")
-categoryLirics.addNew(track: Track(nameTrack: "Shape of My Heart", artist: "Sting", duration: 4.38, country: "GBR"))
-categoryLirics.addNew(track: Track(nameTrack: "Gitan", artist: "Garou", duration: 4.07, country: "FR"))
+//MARK: Проверка №2
+var categoryLirics = Category(name: "Lirics")
+categoryLirics.addNew(track: Track(name: "Shape of My Heart",
+                                   artist: "Sting",
+                                   duration: 4.38,
+                                   country: "GBR"))
+categoryLirics.addNew(track: Track(name: "Gitan",
+                                   artist: "Garou",
+                                   duration: 4.07,
+                                   country: "FR"))
 
 var newLibrary = Library(name: "Моя сборка хитов")
 newLibrary.addNew(category: categoryRock)
@@ -128,5 +177,7 @@ newLibrary.displayAllTracks()
 
 /// Решение данной задачи реализовано в методе transferTrack - задачи №2
 print("\n")
-newLibrary.transferTrack(from: categoryRock, to: categoryLirics, track: 1)
+newLibrary.transferTrack(from: categoryRock,
+                         to: categoryLirics,
+                         track: 4)
 newLibrary.displayAllTracks()
